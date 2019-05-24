5f93e8dec33bbf4bc020d2c71330c43674a93305
Alexandre Ferreira <ferreiramgbr@gmail.com>
2019-05-24 13:59:46 -0300

    Aprendendo poo - Entendo funcionamento dos metodos

---
+++ Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_AprendendoPoo
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public bool viva = true;
+
+         public bool EstaViva (string vive)
+        {
+           if (vive = "não")
+            {
+                this.viva = false;
+                
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_AprendendoPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.nome = "Alexande Mateus dos Santos Ferreira";
+            p1.idade = 21;
+            p1.viva = "não";
+
+            Console.WriteLine(p1.viva);
+
+        }
+    }
+}
